2a7659ac7c008f5b589afa21562bb5c1e6331f57
ruanpc <ruanpingcheng@gmail.com>
2020-09-05 13:34:10 +0800

    finish literature to execution layer

---
+++ chart/intro/script/config.py
@@ -0,0 +1,16 @@
+
+import matplotlib as mpl
+mpl.use('Agg')
+
+mpl.rcParams['legend.fontsize'] = 16
+mpl.rcParams["legend.labelspacing"] = 0.4
+mpl.rcParams["legend.columnspacing"] = 0.4
+mpl.rcParams["legend.handletextpad"] = 0.4
+
+mpl.rcParams['axes.titlesize'] = 30
+mpl.rcParams['axes.titleweight'] = "bold"
+mpl.rcParams['axes.labelsize']=20
+mpl.rcParams['font.size'] = 14
+
+mpl.rcParams['pdf.fonttype'] = 42
+mpl.rcParams['ps.fonttype'] = 42
